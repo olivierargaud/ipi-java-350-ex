72925e3425c0c0f37f6b87770ece4b9e5aba55bf
oargaud <olivierargaud@gmail.com>
2021-02-09 14:49:54 +0100

    EmployeRepositoryTest testFindLastMatricule corrigé 1

--- src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
@@ -3,7 +3,7 @@ package com.ipiecoles.java.java350.repository;
 
 import com.ipiecoles.java.java350.model.Employe;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -18,30 +18,101 @@ class EmployeRepositoryTest {
     EmployeRepository employeRepository;
 
 
+//    @BeforeAll // Junit 5
+//    public void setUp(){//Nom setUp arbitraire
+//        //Appelé une seule fois avant l'exécution des tests
+//    }
+//    @BeforeEach // Junit 5
+//    public void before(){//Nom before arbitraire
+//        //Appelé avant chaque test
+//        employeRepository.deleteAll();
+//    }
+//    @AfterEach //Junit 5
+//    public void after(){//Nom after arbitraire
+//        //Appelé après chaque test
+//        employeRepository.deleteAll();
+//    }
+//    @AfterAll // Junit 5
+//    public void tearDown(){//Nom tearDown arbitraire
+//        //Appelé une fois que tous les tests sont passés
+//    }
+
+    @BeforeEach // Junit 5
+    @AfterEach //Junit 5
+    public void purgeBDD()
+    {
+        employeRepository.deleteAll();
+    }
+
     @Test
-    public void testFindLastMatricule()
+    public void testFindLastMatricule0Employe()
     {
         //given
-        Employe employe = employeRepository.save(new Employe("Doe", "John", "T12345",
-                LocalDate.now().minusYears(5), 1500d, 1, 1.0));
-        Employe employe2 = employeRepository.save(new Employe("Doe2", "John", "T23456",
-                LocalDate.now().minusYears(5), 1500d, 1, 1.0));
-        Employe employe3 = employeRepository.save(new Employe("Doe3", "John", "T34567",
-                LocalDate.now().minusYears(5), 1500d, 1, 1.0));
-        Employe employe4 = employeRepository.save(new Employe("Doe4", "John", "T12389",
-                LocalDate.now().minusYears(5), 1500d, 1, 1.0));
-        Employe employe5 = employeRepository.save(new Employe("Doe5", "John", "TBCDEF",
-                LocalDate.now().minusYears(5), 1500d, 1, 1.0));
-        Employe employe6 = employeRepository.save(new Employe("Doe5", "John", "TABCDE",
+
+        //when
+        String lastMatricule = employeRepository.findLastMatricule();
+
+        //then
+        Assertions.assertThat(lastMatricule).isNull();
+    }
+
+    @Test
+    public void testFindLastMatricule1Employe()
+    {
+        //given
+        employeRepository.save(new Employe("Doe", "John", "T12345",
                 LocalDate.now().minusYears(5), 1500d, 1, 1.0));
 
         //when
         String lastMatricule = employeRepository.findLastMatricule();
 
+        //then
+        Assertions.assertThat(lastMatricule).isEqualTo("12345");
+    }
+
+    @Test
+    public void testFindLastMatriculeNEmploye()
+    {
+        //given
+        employeRepository.save(new Employe("Doe", "John", "T12345",
+                LocalDate.now().minusYears(5), 1500d, 1, 1.0));
+        employeRepository.save(new Employe("Doe2", "John", "T23456",
+                LocalDate.now().minusYears(5), 1500d, 1, 1.0));
+        employeRepository.save(new Employe("Doe3", "John", "T34567",
+                LocalDate.now().minusYears(5), 1500d, 1, 1.0));
+        employeRepository.save(new Employe("Doe4", "John", "T12389",
+                LocalDate.now().minusYears(5), 1500d, 1, 1.0));
+        employeRepository.save(new Employe("Doe5", "John", "TBCDEF",
+                LocalDate.now().minusYears(5), 1500d, 1, 1.0));
+        employeRepository.save(new Employe("Doe5", "John", "TABCDE",
+                LocalDate.now().minusYears(5), 1500d, 1, 1.0));
+        employeRepository.save(new Employe("Doe6", "John", "C40325",
+                LocalDate.now().minusYears(5), 1500d, 1, 1.0));
+        employeRepository.save(new Employe("Doe7", "John", "M06432",
+                LocalDate.now().minusYears(5), 1500d, 1, 1.0));
+
+        //when
+        String lastMatricule = employeRepository.findLastMatricule();
 
         //then
         Assertions.assertThat(lastMatricule).isEqualTo("BCDEF");
     }
+
+
+
+    @Test
+    public void testAvgPerformanceWhereMatriculeStartsWith()
+    {
+
+
+
+
+
+    }
+
+
+
+
 
 
 }
---
+++ src/test/java/com/ipiecoles/java/java350/service/EmployeServiceTest.java
@@ -0,0 +1,88 @@
+package com.ipiecoles.java.java350.service;
+
+import com.ipiecoles.java.java350.exception.EmployeException;
+import com.ipiecoles.java.java350.model.Employe;
+import com.ipiecoles.java.java350.model.NiveauEtude;
+import com.ipiecoles.java.java350.model.Poste;
+import com.ipiecoles.java.java350.repository.EmployeRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+@SpringBootTest
+class EmployeServiceTest {
+
+    @Autowired
+    private EmployeService employeService;
+    @Autowired
+    private EmployeRepository employeRepository;
+
+//    @Test
+//    public void test() throws EmployeException {
+//        //Given
+//        employeRepository.save(new Employe("Doe", "John", "T12345",
+//                LocalDate.now().minusYears(5), 1500d, 1, 1.0));
+//        String nom = "Doe2";
+//        String prenom = "John";
+//        Poste poste = Poste.COMMERCIAL;
+//        NiveauEtude niveauEtude = NiveauEtude.CAP;
+//        Double tempsPartiel = 1.0;
+//
+//        //When
+//        employeService.embaucheEmploye(nom,prenom,poste,niveauEtude,tempsPartiel);
+//        Employe employe1 = employeRepository.findByMatricule("C12346");
+//
+//        //Then
+//        Employe employe = new Employe("Doe2", "John", "C12346",
+//                LocalDate.now(), 1521.22, 1, 1.0);
+//
+//        Assertions.assertThat(employe1).isNotNull();
+//        Assertions.assertThat(employe1.getSalaire()).isEqualTo(1521.22);
+//    }
+
+
+    @Test
+    public void testEmbauchePremierEmploye() throws EmployeException {
+        //Given
+        String nom = "Doe2";
+        String prenom = "John";
+        Poste poste = Poste.TECHNICIEN;
+        NiveauEtude niveauEtude = NiveauEtude.BTS_IUT;
+        Double tempsPartiel = 1.0;
+
+        //When
+        employeService.embaucheEmploye(nom,prenom,poste,niveauEtude,tempsPartiel);
+
+        //Then
+
+//        List<Employe> employes = employeRepository.findAll();
+//        Assertions.assertThat(employes).hasSize(1);
+//        Employe employe = employeRepository.findAll().get(0);
+
+        Employe employe = employeRepository.findByMatricule("T00001");
+        Assertions.assertThat(employe).isNotNull();
+
+        Assertions.assertThat(employe.getNom()).isEqualTo(nom);
+        Assertions.assertThat(employe.getPrenom()).isEqualTo(prenom);
+        Assertions.assertThat(employe.getSalaire()).isEqualTo(1825.46);
+        Assertions.assertThat(employe.getTempsPartiel()).isEqualTo(tempsPartiel);
+        Assertions.assertThat(employe.getDateEmbauche()).isEqualTo(LocalDate.now());
+        Assertions.assertThat(employe.getMatricule()).isEqualTo("T00001");
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
